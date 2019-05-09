139f6f7c627040ca1c9e6c466ed320491df26d76
Fernando Juarez <fhereduardo90@gmail.com>
2019-05-08 22:28:28 -0600

    change algorithms for exercices 1 and 3

--- problem_1.py
+++ problem_1.py
@@ -1,10 +1,9 @@
 def sum_multiples(num):
-    sum = 0
-    for i in range(1, num):
-        if i % 3 == 0 or i % 5 == 0:
-            sum += i
-    else:
-        return sum
+    m_5 = (i for i in range(5, num, 5))
+    m_3 = (i for i in range(3, num, 3))
+    m_15 = (i for i in range(15, num, 15))
+
+    return sum(set(m_15).symmetric_difference(m_3) | set(m_15).symmetric_difference(m_5))
 
 
 print(sum_multiples(1000))
--- problem_3.py
+++ problem_3.py
@@ -1,27 +1,36 @@
 def is_prime(number):
     if number < 2:
         return False
-
-    for i in range(2, number):
-        if number % i == 0:
-            return False
-
-    return True
+    elif number == 2 or number == 3 or number == 5 or number == 7:
+        return True
+    elif number % 2 == 0 or number % 3 == 0 or number % 5 == 0 or number % 7 == 0:
+        return False
+    else:
+        return True
 
 
 def largest_prime_factor(number):
-    prime_factors = []
-    result = 1
+    primes = []
+    division = number
+    multiplier = 1
+    current_prime = 2
+    go = True
 
-    for i in range(number):
-        if is_prime(i) and number % i == 0:
-            prime_factors.append(i)
-            result = result * i
+    while(go):
+        if is_prime(current_prime) and division % current_prime == 0:
+            division = division // current_prime
+            primes.append(current_prime)
+            multiplier = multiplier * current_prime
+        else:
+            current_prime += 1
 
-            if result == number:
-                break
-
-    print(prime_factors)
+        if multiplier >= number:
+            go = False
+        elif current_prime == division:
+            primes.append(division)
+            go = False
+    else:
+        print(primes)
 
 
 largest_prime_factor(600851475143)
